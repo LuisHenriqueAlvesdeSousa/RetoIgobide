0a8830c3e3229d5eb418ad2cd0a6f80e0459a7aa
Aitor <2gdaw02@a224p02.arriagainfo.local>
2022-01-28 08:36:08 +0100

    controler

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UsuarioController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('usuario/', 'UsuarioController@index');
+Route::get('usuario/{id}', 'UsuarioController@show');
+Route::get('usuario/{id}/create', 'UsuarioController@create');
+Route::get('usuario/', 'UsuarioController@store');
