97b1d53176870f5c63e6bc6588d87b852c837dac
Carlos230600 <60777696+Carlos230600@users.noreply.github.com>
2022-02-03 00:27:30 +0100

    listadoTecnicos v2

--- resources/views/listadoTecnicos.blade.php
+++ resources/views/listadoTecnicos.blade.php
@@ -6,13 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Listado Tecnicos</title>
     <style>
-        *{
+      *{
             font-size: 1.35pc;
             font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
         }
         .mainListado{
-            width: 96%;
-            margin: 2%;
+            width: 100%;
             height: auto;
         }
 
@@ -23,7 +22,25 @@
         }
 
         .mainListado li{
-            margin: 1.5% 0;
+            margin: 2% 0;
+        }
+
+        .title{
+            display:flex;
+            align-items:center;
+            justify-content:center;
+        }
+
+        .title1{
+            color: steelblue;
+            font-weight: 500;
+            text-decoration: none;
+        }
+        
+        .title ol{
+            margin-bottom:0;
+            display: flex;
+            justify-content: center;
         }
 
         .tecnicosOpc{
@@ -35,6 +52,14 @@
             justify-content: center;
             align-items: center;
             box-shadow: 0px 0px 2px grey;
+        }
+
+        #creaTecnico{
+            background: linear-gradient(rgb(164, 228, 122), rgb(148, 202, 111));
+        }
+
+        #creaTecnico svg{
+            margin: 0 2%;
         }
 
         .imagenTecn{
@@ -88,14 +113,32 @@
     @include('header')
 
     <div class="mainListado">
-        <ul>
-            <li><div class="tecnicosOpc">Listado de Tecnicos</div></li>
-            <li><div class="tecnicosOpc" onclick="location.href='./nuevoTecnico'">Crear nuevo técnico</div></li>
+         <ul>
+            <li>
+                <div class="title">
+                    <nav aria-label="breadcrumb">
+                      <ol class="breadcrumb">
+                        <li class="breadcrumb-item">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-house" viewBox="0 0 15 20">
+                              <path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
+                              <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
+                            </svg>
+                        </li>
+                        <li class="breadcrumb-item">Tecnicos</li>
+                      </ol>
+                    </nav>
+                </div>
+            </li>
+            <li><div id="creaTecnico" class="tecnicosOpc" onclick="location.href='./nuevoTecnico'">
+                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-person-plus" viewBox="0 0 16 16">
+                    <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+                    <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
+                </svg>
+                Crear nuevo técnico</div>
+            </li>
         </ul>
 
-        <p class="title">Lista de Técnicos</p>
-        <hr>
-
+        <a class="title1">Lista de Técnicos</a>
         <!--Litado de los tecnicos-->
         <ul>
             <li class="tecnicosOpc2"> <!--Tecnico 1-->
