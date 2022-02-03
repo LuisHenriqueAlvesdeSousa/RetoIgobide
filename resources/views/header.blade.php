bbe0c1a9e4015068026a6e623088856bc75c0fde
Carlos230600 <60777696+Carlos230600@users.noreply.github.com>
2022-02-03 02:38:59 +0100

    Update header.blade.php

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -41,7 +41,7 @@
 
         #logo{
             width: 58.5%;
-            background-image: url("./logo.png");
+            background-image: url("{{url('/logo.jpg')}}");
             background-size: contain;
             background-repeat: no-repeat;
             background-position: center;
