7798eebe97dd10b9771fbf4a2316a008e0756d83
Carlos230600 <60777696+Carlos230600@users.noreply.github.com>
2022-02-03 13:01:03 +0100

    Eliminado Boton Ubicacion actual

--- resources/views/detalleIncidencia.blade.php
+++ resources/views/detalleIncidencia.blade.php
@@ -194,54 +194,13 @@
         async function initMap(){//Funcion asincrona
             
         var posiciones=await geocode();//lat y lng para centrar el mapa en la calle requerida await funcion de obtencion de pos.
-
-        console.log(posiciones);
         map = new google.maps.Map(document.getElementById("map"), {
             center: { lat: posiciones[0], lng: posiciones[1] },
             zoom: 18,
         });
         infoWindow = new google.maps.InfoWindow();
-
-        const locationButton = document.createElement("button");
-
-        locationButton.textContent = "Ir a mi ubicacion actual";
-        locationButton.classList.add("custom-map-control-button");
-        map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
-        locationButton.addEventListener("click", () => {
-            // Try HTML5 geolocation.
-            if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(
-                (position) => {
-                const pos = {
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude,
-                };
-
-                infoWindow.setPosition(pos);
-                //infoWindow.setContent("Ubicacion encontrada");
-                //infoWindow.open(map);
-                map.setCenter(pos);
-                },
-                () => {
-                handleLocationError(true, infoWindow, map.getCenter());
-                }
-            );
-            } else {
-            // Browser doesn't support Geolocation
-            handleLocationError(false, infoWindow, map.getCenter());
-            }
-        });
         }
-
-        function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(
-            browserHasGeolocation
-            ? "Error: The Geolocation service failed."
-            : "Error: Your browser doesn't support geolocation."
-        );
-        infoWindow.open(map);
-        }
+       
     </script>
     <script src="./jquery-3.6.0.min.js"></script>
     <script src="./sweetalert2.all.min.js"></script>
