40cbcb27f810a7233115940058461251e76b0d93
Carlos230600 <60777696+Carlos230600@users.noreply.github.com>
2022-02-03 01:09:30 +0100

    Update nuevaIncidencia.blade.php

--- resources/views/nuevaIncidencia.blade.php
+++ resources/views/nuevaIncidencia.blade.php
@@ -6,14 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Nueva Incidencia</title>
     <style>
-           *{
+        *{
             font-size: 1.2pc;
             font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
         }
         .newOperario{
             width: 96%;
             height: auto;
-            margin: 2%;
+            margin: 0 2%;
         }
         .newOperario ul{
             list-style: none;
@@ -23,6 +23,18 @@
 
         .newOperario li{
             margin: 4% 0;
+        }
+
+        .title{
+            display:flex;
+            align-items:center;
+            justify-content:center;
+        }
+        
+        .title ol{
+            margin-bottom:0;
+            display: flex;
+            justify-content: center;
         }
 
         .opNewOperario{
@@ -45,6 +57,7 @@
         .titulo{
             color: grey;
             font-weight: 500;
+            text-decoration: none;
         }
 
         #controles{
@@ -62,7 +75,7 @@
         }
 
         #guardar{
-            background: linear-gradient(rgb(182, 255, 175), rgb(145, 223, 151));
+            background: linear-gradient(rgb(164, 228, 122), rgb(148, 202, 111));
         }
 
         .selector{
@@ -130,15 +143,28 @@
             font-weight: normal;
         }
     </style>
-    <script src="{{ url('https://cdn.jsdelivr.net/npm/sweetalert2@11.3.10/dist/sweetalert2.all.min.js') }}"></script>
-    <script src="{{ url('https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js') }}"></script>
 </head>
 <body>
     @include('header')
 
-    <div class="newOperario">
+     <div class="newOperario">
         <ul>
-            <li><div class="opNewOperario" style="background: linear-gradient(rgb(214, 231, 245), lightsteelblue);">Crear Nueva Incidencia</div></li>
+            <li>
+                <div class="title">
+                    <nav aria-label="breadcrumb">
+                      <ol class="breadcrumb">
+                        <li class="breadcrumb-item">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-house" viewBox="0 0 15 20">
+                              <path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
+                              <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
+                            </svg>
+                        </li>
+                        <li class="breadcrumb-item">Incidencias</li>
+                        <li class="breadcrumb-item">Nueva Incidencia</li>
+                      </ol>
+                    </nav>
+                </div>
+            </li>
             <li>
                 <div class="opNewOperario2">
                     <a class="titulo">Nombre*</a>
@@ -172,7 +198,7 @@
             <li>
                 <div class="opNewOperario2">
                     <a class="titulo">Comentario</a>
-                    <input type="text" class="opNewOperarioComentario">
+                    <textarea type="text" class="opNewOperarioComentario" maxlength="250" placeholder="Maximo 250 caracteres"></textarea>
                 </div>
             </li>
 
@@ -221,7 +247,6 @@
             <button class="botControl" id="cancelar">Cancelar</button>
         </div>
     </div>
-
     <script>
         $(document).ready(function(){
             var tecnicos = ["Luis Henrique", "Aitor Molano", "Carlos Garcia"];
