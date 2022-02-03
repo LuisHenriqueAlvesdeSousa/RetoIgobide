7cc45f0d66b3031b83928ce40cbf04a4e9d57b12
Carlos230600 <60777696+Carlos230600@users.noreply.github.com>
2022-02-03 12:55:45 +0100

    Update detalleIncidencia.blade.php

--- resources/views/detalleIncidencia.blade.php
+++ resources/views/detalleIncidencia.blade.php
@@ -296,68 +296,70 @@
 
     @include('header')
 
-   <div class="container">
-        <div class="col">
-            <!--Añadir las clases de bootstrap-->
-            <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12 maindetalleIncidencia">
-                <ul>
-                    <li>
-                        <div class="title">
-                            <nav aria-label="breadcrumb">
-                              <ol class="breadcrumb">
-                                <li class="breadcrumb-item">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-house" viewBox="0 0 16 20">
-                                      <path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
-                                      <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
-                                    </svg>
-                                </li>
-                                 <li class="breadcrumb-item">Incidencias</li>
-                                <li class="breadcrumb-item active" aria-current="page">Detalle Incidencia</li>
-                              </ol>
-                            </nav>
+    <div class="maindetalleIncidencia">
+        <div class="flex-container">
+            <div class="col">
+                    <!--Añadir las clases de bootstrap-->
+                <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+                    <ul>
+                        <li>
+                            <div class="title">
+                                <nav aria-label="breadcrumb">
+                                    <ol class="breadcrumb">
+                                    <li class="breadcrumb-item">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-house" viewBox="0 0 16 20">
+                                            <path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
+                                            <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
+                                        </svg>
+                                    </li>
+                                        <li class="breadcrumb-item">Incidencias</li>
+                                    <li class="breadcrumb-item active" aria-current="page">Detalle Incidencia</li>
+                                    </ol>
+                                </nav>
+                            </div>
+                        </li>
+                        <li><div class="manOpc manOpc2">
+                            <div id="map"></div>
+                            <!-- Async script executes immediately and must be after any DOM elements used in callback. -->
+                            <script
+                                src="https://maps.googleapis.com/maps/api/js?key=AIzaSyALUqJpjrUqrUZaDmf5akxGk01EeU-1Do0&callback=initMap&v=weekly"
+                                async
+                            ></script>
+                        </div></li>
+                                <li>
+                            <a class="titulo1">Descripción</a>
+                            <p id="descripcion"></p>
+                        </li>
+                        <li>
+                            <a class="titulo1">Detalles</a>
+                            <div class="manOpc3">
+                                <ul id="lDetalles"></ul>
+                            </div>
+                        </li>
+                        <li>
+                            <a class="titulo1">Comentarios</a>
+                            <div id="manOpc4" class="manOpc manOpc4">
+                                <!--Cargar los comentarios-->
+                                <ul id="lcomentarios"></ul>
+                            </div>
+                        </li>
+                    </ul>
+                        
+                    <div id="addComent" class="manOpc">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-chat-text" viewBox="0 0 16 16">
+                            <path d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z"/>
+                            <path d="M4 5.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zM4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8zm0 2.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5z"/>
+                        </svg>
+                        Añadir comentario
+                    </div>
+                    <div id="comentario" style="display: none; height: auto;">
+                        <textarea class="form-control" maxlength="150" rows="2" placeholder="Escribe aqui tu comentario"></textarea>
+                        <!--Añadir comentario-->
+                        <div id="enviar">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-send" viewBox="0 0 16 16">
+                                <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z"/>
+                            </svg> Enviar
                         </div>
-                    </li>
-                    <li><div class="manOpc manOpc2">
-                        <div id="map"></div>
-                        <!-- Async script executes immediately and must be after any DOM elements used in callback. -->
-                        <script
-                          src="https://maps.googleapis.com/maps/api/js?key=AIzaSyALUqJpjrUqrUZaDmf5akxGk01EeU-1Do0&callback=initMap&v=weekly"
-                          async
-                        ></script>
-                    </div></li>
-                         <li>
-                        <a class="titulo1">Descripción</a>
-                        <p id="descripcion"></p>
-                    </li>
-                    <li>
-                        <a class="titulo1">Detalles</a>
-                        <div class="manOpc3">
-                            <ul id="lDetalles"></ul>
-                        </div>
-                    </li>
-                    <li>
-                        <a class="titulo1">Comentarios</a>
-                        <div id="manOpc4" class="manOpc manOpc4">
-                            <!--Cargar los comentarios-->
-                            <ul id="lcomentarios"></ul>
-                        </div>
-                    </li>
-                </ul>
-                    
-                <div id="addComent" class="manOpc">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-chat-text" viewBox="0 0 16 16">
-                        <path d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z"/>
-                        <path d="M4 5.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zM4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8zm0 2.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5z"/>
-                    </svg>
-                    Añadir comentario
-                </div>
-                <div id="comentario" style="display: none; height: auto;">
-                    <textarea class="form-control" maxlength="150" rows="2" placeholder="Escribe aqui tu comentario"></textarea>
-                    <!--Añadir comentario-->
-                    <div id="enviar">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-send" viewBox="0 0 16 16">
-                            <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z"/>
-                        </svg> Enviar
                     </div>
                 </div>
             </div>
