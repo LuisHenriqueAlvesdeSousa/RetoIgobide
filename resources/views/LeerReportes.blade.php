82754071364c4674ba5cc1213199aca0cf39eca1
Carlos230600 <60777696+Carlos230600@users.noreply.github.com>
2022-02-03 02:14:04 +0100

    Update LeerReportes.blade.php

--- resources/views/LeerReportes.blade.php
+++ resources/views/LeerReportes.blade.php
@@ -50,10 +50,22 @@
             margin-left: 2%;
         }
 
-        #title{
-            text-align: center;
-            padding: 2% 0 0 0;
-            margin-bottom: 0;
+        .title{
+            display:flex;
+            align-items:center;
+            justify-content:center;
+        }
+
+        .title1{
+            color: steelblue;
+            font-weight: 500;
+            text-decoration: none;
+        }
+        
+        .title ol{
+            margin-bottom:0;
+            display: flex;
+            justify-content: center;
         }
 
     </style>
@@ -64,12 +76,26 @@
 
     <div class="mainReportes">
         <ul>
-            <li><div class="reportesOpc">Leer Reportes</div></li>
+            <li>
+                <div class="title">
+                    <nav aria-label="breadcrumb">
+                      <ol class="breadcrumb">
+                        <li class="breadcrumb-item">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-house" viewBox="0 0 15 20">
+                              <path fill-rule="evenodd" d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>
+                              <path fill-rule="evenodd" d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"/>
+                            </svg>
+                        </li>
+                        <li class="breadcrumb-item">Reportes</li>
+                      </ol>
+                    </nav>
+                </div>
+            </li>
             <li><div class="reportesOpc">Filtros</div></li>
             <li>
                 <div>    
-                    <p id="title">Listado de reportes</p>
-                    <hr>
+                    <a class="title1">Listado de reportes</a>
+                  
 
                     <ul class="lReportes">
                         <li><!--Reporte1-->
