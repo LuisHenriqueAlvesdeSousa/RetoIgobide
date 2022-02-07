d3bb7968a6ea5eae223b2204b29e7830570fac77
Luis Henrique Alves de Sousa <73885520+LuisHenriqueAlvesdeSousa@users.noreply.github.com>
2022-02-07 12:45:12 +0100

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,54 +1,28 @@
 <?php
 
 namespace App\Providers;
-   
-use Illuminate\Cache\RateLimiting\Limit;
+
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\RateLimiter;
-use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Str;
-   
+
 class AppServiceProvider extends ServiceProvider
 {
-    
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
     public function register()
-        {
-            //
-        }
-    public const HOME = '/home';
+    {
+        //
+    }
 
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
     public function boot()
     {
-        $this->removeIndexFromUrl();
-   
-        $this->routes(function () {
-            Route::prefix('api')
-                ->middleware('api')
-                ->namespace($this->namespace)
-                ->group(base_path('routes/api.php'));
-  
-            Route::middleware('web')
-                ->namespace($this->namespace)
-                ->group(base_path('routes/web.php'));
-        });
+        //
     }
-   
-    /**
-     * Write code on Method
-     *
-     * @return response()
-     */
-    protected function removeIndexFromUrl()
-    {
-        if (Str::contains(request()->getRequestUri(), '/index.php/')) {
-            $url = str_replace('index.php/', '', request()->getRequestUri());
-   
-            if (strlen($url) > 0) {
-                header("Location: $url", true, 301);
-                exit;
-            }
-        }
-    }
-   
 }
