43a5898ea2413aea6e9c6cc70e48e9a7396d4912
Luis Henrique Alves de Sousa <73885520+LuisHenriqueAlvesdeSousa@users.noreply.github.com>
2022-02-07 10:46:16 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/menujefe',function(){
     return view('menujefe'); 
 })->name('menu');
 
-Route::post('/login', [HomeController::class, 'index'])->name('login');
+//Route::post('/login', [HomeController::class, 'index'])->name('login');
 
 Route::get('/reportes',function(){
     return view('LeerReportes'); 
