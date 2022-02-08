55d219b004291a1fe5a625fc512b1b5b49ca5573
Luis Henrique Alves de Sousa <73885520+LuisHenriqueAlvesdeSousa@users.noreply.github.com>
2022-02-08 09:24:24 +0100

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -35,7 +35,7 @@ class HomeController extends Controller
         }
         //comprobamos si recibimos parametros para realizar un login
         else{
-            return view('menuJefe');
+            return view('menujefe');
         }
     }
     
