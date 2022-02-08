f9c07a8cfb48b39fe15037801615380300468b38
Luis Henrique Alves de Sousa <73885520+LuisHenriqueAlvesdeSousa@users.noreply.github.com>
2022-02-09 00:43:13 +0100

    Update EstadisticaController.php

--- app/Http/Controllers/EstadisticaController.php
+++ app/Http/Controllers/EstadisticaController.php
@@ -10,23 +10,8 @@ class EstadisticaController extends Controller
 
     public function EstadisticaExport()
     {       
-        if(isset($_POST['fchInicio']) && isset($_POST['fchFin'])){
-            //validaciones
-            $fchInicio = New DateTime($_POST['fchInicio'])/*->format('dd-mm-yyyy')*/;
-            $fchFin = New DateTime($_POST['fchFin'])/*->format('dd-mm-yyyy')*/;
-    
-            $fchControl = New DateTime('02-02-2022')/*->format('dd-mm-yyyy')*/;
-            $fchHoy = New DateTime('now')/*->format('dd-mm-yyyy')*/;
-            
-            if($fchInicio > $fchFin) return redirect('/estadisticas/create')->with('status', 'La fecha de Inicio es mayor que la fecha Final.');
-    
-            if($fchInicio < $fchControl) $fchInicio = $fchControl;
-    
-            if($fchFin > $fchHoy) $fchFin = $fchHoy;
-    
             //generar Excel
-            return Excel::download(new EstadisticaExport($fchInicio, $fchFin), 'estadisticas.xlsx');
-    
-        }
+            return Excel::download(new EstadisticaExport(), 'estadisticas.xlsx');
+
     }
 }
