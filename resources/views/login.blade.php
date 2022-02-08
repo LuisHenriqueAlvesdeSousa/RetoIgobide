5fda8ce2f80fb0260ca1d8bd27fdf80e77965e1d
Luis Henrique Alves de Sousa <73885520+LuisHenriqueAlvesdeSousa@users.noreply.github.com>
2022-02-08 09:03:40 +0100

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -22,6 +22,7 @@
     </style>
     <div id="contLogin">
         <form method="POST" action="index.php">
+            @crsf
             <label for="email">
                 <span>Email Coorporativo</span>
                 <input type="email" id="email" name="email" required>
