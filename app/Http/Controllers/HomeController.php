241405218dc7f87e37831f7907c6e4f59fbc7e2b
Luis Henrique Alves de Sousa <73885520+LuisHenriqueAlvesdeSousa@users.noreply.github.com>
2022-02-08 09:55:36 +0100

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -43,8 +43,7 @@ class HomeController extends Controller
         if(null !== $_POST["email"] && null !== $_POST["password"]){
             $usuarioActual = Users::where('email', '=', $_POST["email"])->first();
             if($usuarioActual->id() != null){
-                echo $usuarioActual;
-                exit;
+                echo 'flag';
                 //return view('login');
             }else{
                 $_SESSION['idUsuario'] = $usuarioActual->id();
