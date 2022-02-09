56121ff0c6ccbd37d10b3e0f382d9abf19532133
Luis Henrique Alves de Sousa <73885520+LuisHenriqueAlvesdeSousa@users.noreply.github.com>
2022-02-09 02:14:14 +0100

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -26,7 +26,8 @@
       <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
         <div class="card border-0 shadow rounded-3 my-5">
           <div class="card-body p-4 p-sm-5">
-            <h5 class="card-title text-center mb-5 fw-light fs-5">Login</h5>
+            <h5 class="card-title text-center mb-5 fw-light fs-5">Espacio de trabajo de Igobide</h5>
+            <h5 class="card-title text-center mb-5 fw-light fs-5">- LOGIN -</h5>
                 <form method="POST" action="index.php">
                     @csrf
                     <div class="form-floating mb-3">
