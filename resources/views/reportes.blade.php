f4b86052e6dab17fc6256d2401a0eec425202a7b
Luis Henrique Alves de Sousa <73885520+LuisHenriqueAlvesdeSousa@users.noreply.github.com>
2022-02-09 02:29:28 +0100

    Update reportes.blade.php

--- resources/views/reportes.blade.php
+++ resources/views/reportes.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
@@ -14,19 +15,26 @@
             </div>
         @endif
 
-    <div id="contReportes">
-
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+        <div class="card border-0 shadow rounded-3 my-5">
+          <div class="card-body p-4 p-sm-5">
                 @foreach ($errors->all() as $error)
                     <p class="alert alert-danger">{{ $error}}</p><br>
                 @endforeach
 
-        <h1>Generador de repotes en Excel.</h1>
+        <h1 class="card-title text-center mb-5 fw-light fs-5">Generador de repotes en Excel.</h1>
         <p>Aquí podrás podras generar un reporte y descargar un archivo Excel (.xsl) en el cual podrás analizar e imprimir los datos con mayor facilidad</p>
 
             <form method="post">
             @csrf
-                <input type="submit" value="Generar Reporte">
+                <div class="d-grid"><input type="submit" class="btn mr-md-2 mb-md-0 mb-2 btn-success" value="Generar Reporte"></div>
             </form>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </body>
 </html>
