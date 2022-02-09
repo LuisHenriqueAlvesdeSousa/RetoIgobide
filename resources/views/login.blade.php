4e04a6475ddefe02fcc1883f47ad756a4049c1e2
Aitor <2gdaw02@a224p02.arriagainfo.local>
2022-02-09 12:03:31 +0100

    login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,6 +7,12 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="./css/app.css" rel="stylesheet">
     <title>Login</title>
+    <style>
+      body{
+        background: #007bff;
+        background: linear-gradient(to right, #0062E6, #33AEFF);
+      }
+    </style>
 </head>
 <body>
   <div class="container">
