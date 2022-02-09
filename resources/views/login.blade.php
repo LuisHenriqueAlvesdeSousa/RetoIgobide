9541007a27bd1193c5e33b1086805874d209528c
Luis Henrique Alves de Sousa <73885520+LuisHenriqueAlvesdeSousa@users.noreply.github.com>
2022-02-09 02:01:33 +0100

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -48,29 +48,30 @@
 
 
     </style>
-<div class="container mt-5">
-    <div class="row d-flex justify-content-center">
-        <div class="col-md-6">
-            <div class="card px-5 py-5">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+        <div class="card border-0 shadow rounded-3 my-5">
+          <div class="card-body p-4 p-sm-5">
+            <h5 class="card-title text-center mb-5 fw-light fs-5">Login</h5>
                 <form method="POST" action="index.php">
                     @csrf
-                        <label for="email">
-                        <div class="forms-inputs mb-4"> 
-                            <span>Email Coorporativo</span>
-                            <input type="email" id="email" name="email" required>
-                        </div>
-                        </label>
-                        <label for="password">
-                        <div class="forms-inputs mb-4">
-                            <span>Contraseña</span>
-                            <input type="password" id="password" name="password" required>
-                        </div>
-                        </label>
-                    <div class="mb-3"><input type="submit" value="Entrar"></div>
+                    <div class="form-floating mb-3">
+                        <input type="email" id="email" name="email" required class="form-control" id="floatingInput" placeholder="nombre@ejemplo.com">
+                        <label for="floatingInput">Email Coorporativo</label>
+                    </div>
+                    
+                    <div class="form-floating mb-3">
+                        <input type="password" id="password" name="password" required class="form-control" id="floatingPassword" placeholder="Contraseña">
+                        <label for="floatingPassword">Contraseña</label>
+                    </div>
+
+                    <div class="d-grid"><button class="btn btn-primary btn-login text-uppercase fw-bold" type="submit">Entrar</button></div>
                 </form>
-            </div>
+          </div>
         </div>
+      </div>
     </div>
-</div>
+  </div>
 </body>
 </html>
