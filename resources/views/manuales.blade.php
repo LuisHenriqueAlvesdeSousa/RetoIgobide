3b39f26d5ccea45658f019305c90b247fe239882
Carlos230600 <60777696+Carlos230600@users.noreply.github.com>
2022-02-10 17:56:46 +0100

    Update manuales.blade.php

--- resources/views/manuales.blade.php
+++ resources/views/manuales.blade.php
@@ -79,6 +79,10 @@
                     right: 3%;
                 }
                 
+                .opc:hover{
+                    cursor:pointer;
+                }
+                
                    @media (max-width:768px) {
                         #listaManuales >li{
                             width: 100%;
@@ -89,7 +93,7 @@
             </style>
             <div class="tecnicosOpc2">
                 <div class="imagenTecn">
-                    <svg onclick="window.location.href=this.getAttribute('ruta')" ruta="${this.getAttribute('ruta')}" xmlns="http://www.w3.org/2000/svg" width="4pc" height="4.1pc" fill="currentColor" class="bi bi-info" viewBox="0 0 16 16">
+                    <svg onclick="window.location.href=this.getAttribute('ruta')" ruta="${this.getAttribute('ruta')}" class="opc"xmlns="http://www.w3.org/2000/svg" width="4pc" height="4.1pc" fill="currentColor" class="bi bi-info" viewBox="0 0 16 16">
                     <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
                     </svg>
                 </div>
@@ -97,7 +101,7 @@
                 <div class="opciones">
                     <ul>
                         <li>
-                            <svg onclick="window.location.href='./eliminarManual/' + this.id" id="${this.getAttribute('idmanual')}" xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                            <svg onclick="window.location.href='./eliminar/' + this.id" id="${this.getAttribute('idmanual')}" class="opc" xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                                 <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
                                 <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
                             </svg>
