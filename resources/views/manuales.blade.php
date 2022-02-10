13b9a1b18352d487f910fd90fccaa5aca165b8b6
Luis Henrique Alves de Sousa <73885520+LuisHenriqueAlvesdeSousa@users.noreply.github.com>
2022-02-10 12:50:20 +0100

    Update manuales.blade.php

--- resources/views/manuales.blade.php
+++ resources/views/manuales.blade.php
@@ -13,6 +13,10 @@
         constructor(){
             super();
         }
+        
+        verManual(){
+            alert("ir a manual");
+        }
 
         connectedCallback(){
             let shadowRoot = this.attachShadow({ mode: "open" });
@@ -89,7 +93,7 @@
             </style>
             <div class="tecnicosOpc2">
                 <div class="imagenTecn">
-                    <svg onclick="window.open(${this.getAttribute("ruta")})" xmlns="http://www.w3.org/2000/svg" width="4pc" height="4.1pc" fill="currentColor" class="bi bi-info" viewBox="0 0 16 16">
+                    <svg onclick="verManual()" xmlns="http://www.w3.org/2000/svg" width="4pc" height="4.1pc" fill="currentColor" class="bi bi-info" viewBox="0 0 16 16">
                     <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
                     </svg>
                 </div>
