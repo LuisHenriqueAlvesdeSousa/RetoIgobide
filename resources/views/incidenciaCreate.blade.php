ca888ab7679cba66c3814637ef0356a764e1d86e
Luis Henrique Alves de Sousa <73885520+LuisHenriqueAlvesdeSousa@users.noreply.github.com>
2022-02-10 08:52:47 +0100

    Update incidenciaCreate.blade.php

--- resources/views/incidenciaCreate.blade.php
+++ resources/views/incidenciaCreate.blade.php
@@ -212,7 +212,7 @@
                 <li>
                     <div class="opNewOperario2">
                         <a class="titulo">Operario</a>
-                        <button class="addOperario" id="addOperario">
+                        <button type='button' class="addOperario" id="addOperario">
                             <svg id="addIcon" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person-plus" viewBox="0 0 16 16">
                                 <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H1s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C9.516 10.68 8.289 10 6 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
                                 <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
@@ -226,8 +226,8 @@
             </ul>
 
             <div id="controles">
-                <button type="submit" class="botControl" id="guardar">Guardar</button>
-                <button class="botControl" id="cancelar">Cancelar</button>
+                <input type="submit" class="botControl" id="guardar">Guardar</button>
+                <button type='button' class="botControl" id="cancelar">Cancelar</button>
             </div>
         </form>
     </div>
