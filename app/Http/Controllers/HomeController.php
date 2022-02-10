fbd55bca897b62e729a53318c0001c5116051251
Luis Henrique Alves de Sousa <73885520+LuisHenriqueAlvesdeSousa@users.noreply.github.com>
2022-02-10 17:14:11 +0100

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -39,7 +39,7 @@ class HomeController extends Controller
         }
         //comprobamos si recibimos parametros para realizar un login
         else{
-            return redirect('../menu');
+            return redirect->route('menu');
         }
     }
     
@@ -73,7 +73,7 @@ class HomeController extends Controller
                     }else{
                         $_SESSION['rol'] = "director";
                     }
-                    return redirect('../menu'); 
+                    return redirect->route('menu');
                 }else{
                     return view('login');
                 }
