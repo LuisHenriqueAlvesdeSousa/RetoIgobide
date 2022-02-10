7751a6fb9c23f6da1a56dbef277245f687a1777b
Luis Henrique Alves de Sousa <73885520+LuisHenriqueAlvesdeSousa@users.noreply.github.com>
2022-02-10 10:06:16 +0100

    Update footer.blade.php

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,3 +1,18 @@
+  <style>
+  /*Footer*/
+  footer{
+    color: white;
+    background-color: black;
+    width: 100%;
+    height: 100px;
+    display:flex;
+}
+
+#contFooter{
+    align-self: center;
+    margin-left: 3em;
+}
+</style>
     <footer>
         <div id="contFooter">
             <span>IGOBIDE ©</span>
